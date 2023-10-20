aa35dd28f7cdf0c9564117962ffa3f3c8f1bae6d
yoga-18 <yoganathan.yo@gmail.com>
2023-10-20 17:52:59 +0530

    FirstCommit

---
+++ src/main/java/com/junitproject/JunitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.junitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junitproject/JunitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
